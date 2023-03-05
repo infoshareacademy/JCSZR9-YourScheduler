8afaeffbe7d835b85b136a8a24a6009e4ce10f13
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-03-05 12:41:15 +0100

    Created Event Template with basic properties and AddUser method

---
+++ YourScheduler/YourScheduler.BusinessLogic/Event.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourScheduler.BusinessLogic
+{
+    internal class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public List<User> Participants { get; set; }
+        public bool IsOpen { get; set; }
+
+        public Event(string name, string description, DateTime date, List<User> participants, bool isopen)
+        {
+            Name = name;
+            Description = description;
+            Date = date;
+            Participants = participants;
+            IsOpen = isopen;
+        }
+
+        public void AddUser(User user)
+        {
+            Participants.Add(user);
+        }
+
+
+
+
+
+    }
+}
