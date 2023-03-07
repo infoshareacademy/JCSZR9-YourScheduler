716a0ab7317d6200245cfef61e7ec48635b41f28
Mariusz Pudlik <marpudlik@wp.pl>
2023-03-07 19:38:32 +0100

    Changing in main program for using none static methods

--- YourScheduler/YourScheduler.ConsoleApp/Menu.cs
+++ YourScheduler/YourScheduler.ConsoleApp/Menu.cs
@@ -9,32 +9,34 @@ namespace YourScheduler.ConsoleApp
 {
     public class Menu
     {
-        private UsersTemporaryList _usersStore = new UsersTemporaryList();
+       // private User _usersStore = new User();
         private CliHelper _cliHelper = new CliHelper();
         internal void RunMenu()
         {
             string loggedUser = "marpudlik@wp.pl";
             Console.WriteLine("Witaj! Aplikacja YourScheduler");
 
-            for (int i = 0; i < 4; i++)
-            {
-                Console.WriteLine($"Podaj {i + 1} user");
-                UserTemporary user = new UserTemporary
-                {
-                    Id = i + 1,
-                    Name = _cliHelper.GetStringFromUser("Get name of User"),
-                    Surname = _cliHelper.GetStringFromUser("Get surname of User"),
-                    Email = _cliHelper.GetStringFromUser("Get email"),
-                    Password = _cliHelper.GetStringFromUser("Password")
-                };
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine($"Podaj {i + 1} user");
+            //    UserTemporary user = new UserTemporary
+            //    {
+            //        Id = i + 1,
+            //        Name = _cliHelper.GetStringFromUser("Get name of User"),
+            //        Surname = _cliHelper.GetStringFromUser("Get surname of User"),
+            //        Email = _cliHelper.GetStringFromUser("Get email"),
+            //        Password = _cliHelper.GetStringFromUser("Password")
+            //    };
 
-                _usersStore.users.Add(user);
-            }
+            //    _usersStore.users.Add(user);
+            //}
 
             Console.WriteLine("Choose operation");
             Console.WriteLine("1 - Show User Profile");
             Console.WriteLine("2 - Show Teams");
             Console.WriteLine("3 - Show Events");
+            Console.WriteLine("4 - Add Event");
+            Console.WriteLine("5 - Add User");
             ChooseOperation(loggedUser);
         }
 
@@ -43,8 +45,8 @@ namespace YourScheduler.ConsoleApp
             int operation;
             do
             {
-                operation = _cliHelper.GetIntFromUser("Get number of operation 1 or 2 or 3");
-            } while (operation <= 0 || operation > 3);
+                operation = _cliHelper.GetIntFromUser("Choose number of operation 1, 2, 3, 4, 5");
+            } while (operation <= 0 || operation > 5);
 
             switch (operation)
             {
@@ -57,6 +59,10 @@ namespace YourScheduler.ConsoleApp
                 case 3:
                     ShowEvents();
                     break;
+                case 4:
+                    break;
+                case 5:
+                    break;
                 default:
                     Console.WriteLine("You get wrong number operation");
                     break;
@@ -65,25 +71,25 @@ namespace YourScheduler.ConsoleApp
 
         void ShowUserProfile(string loggedUser)
         {
-            bool ifUserExist = false;
-            foreach (var user in _usersStore.users)
-            {
-                if (loggedUser == user.Email)
-                {
-                    ifUserExist = true;
-                    Console.WriteLine("It is profil logged user: ");
-                    Console.Write($"{user.Name}");
-                    Console.WriteLine($"{user.Surname}");
-                    Console.WriteLine($"{user.Email}");
-                    Console.WriteLine($"{user.Password}");
-                    return;
-                }
+            //bool ifUserExist = false;
+            //foreach (var user in _usersStore.users)
+            //{
+            //    if (loggedUser == user.Email)
+            //    {
+            //        ifUserExist = true;
+            //        Console.WriteLine("It is profil logged user: ");
+            //        Console.Write($"{user.Name}");
+            //        Console.WriteLine($"{user.Surname}");
+            //        Console.WriteLine($"{user.Email}");
+            //        Console.WriteLine($"{user.Password}");
+            //        return;
+            //    }
 
-            }
-            if (ifUserExist == false)
-            {
-                Console.WriteLine($"User {loggedUser} Doesn't exist");
-            }
+            //}
+            //if (ifUserExist == false)
+            //{
+            //    Console.WriteLine($"User {loggedUser} Doesn't exist");
+            //}
 
         }
 
