b63aa312adc8f96c2020b24c28ad66ceb99ee787
Mariusz Pudlik <marpudlik@wp.pl>
2023-03-07 19:17:40 +0100

    Add CliHelper Class, to parse data from keybord

---
+++ YourScheduler/YourScheduler.ConsoleApp/CliHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourScheduler.ConsoleApp
+{
+    internal class CliHelper
+    {
+        private string GetInfoFromUser()
+        {
+            string infoFromKeyboard = "";
+            infoFromKeyboard = Console.ReadLine();
+            return infoFromKeyboard;
+        }
+
+        internal string GetStringFromUser(string message)
+        {
+            string infoFromUser = "";
+            Console.WriteLine($"{message}");
+            bool correctValue = false;
+            do
+            {
+                infoFromUser = GetInfoFromUser();
+                if (infoFromUser.Length == 0)
+                {
+                    correctValue = false;
+                    Console.WriteLine("You have to get not empty string");
+                }
+                else
+                {
+                    correctValue = true;
+                }
+            } while (correctValue != true);
+            return infoFromUser;
+        }
+
+        internal int GetIntFromUser(string message)
+        {
+            bool intCorrectValue = false;
+            int result = 0;
+            do
+            {
+                Console.WriteLine($"{message}");
+                intCorrectValue = int.TryParse(GetInfoFromUser(), out result);
+            } while (intCorrectValue != true || result == 0);
+
+            return result;
+        }
+
+    }
+}
+
