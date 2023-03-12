79df255828e6d8b2c779a83f69e396d83e287a6c
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-03-12 12:47:32 +0100

    Added Unique Id to Users.cs and CreateEvent

--- YourScheduler/YourScheduler.BusinessLogic/Event.cs
+++ YourScheduler/YourScheduler.BusinessLogic/Event.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace YourScheduler.BusinessLogic
 {
-    internal class Event
+    public class Event
     {
         public int Id { get; set; }
         public string Name { get; set; }
