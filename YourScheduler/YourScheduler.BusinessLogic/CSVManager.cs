730dc2d507b50033df38308466ae028817b0ec43
Krzysiek Gesicki <krzysiek.gesicki@gmail.com>
2023-03-16 21:53:14 +0100

    added methods for working with user and event file

--- YourScheduler/YourScheduler.BusinessLogic/CSVManager.cs
+++ YourScheduler/YourScheduler.BusinessLogic/CSVManager.cs
@@ -24,18 +24,13 @@ namespace YourScheduler.BusinessLogic
         }
         public static List<User> GetUsers()
         {
-            List<User> users = new List<User> ();
+            List<User> users = new List<User>();
             string[] linesFromCSV = System.IO.File.ReadAllLines(GetUsersFilePath());
             foreach (var line in linesFromCSV)
             {
-                User newUser = new User();
                 string[] splittedData = line.Split(',');
-                newUser.Id = Convert.ToInt32(splittedData[0]);
-                newUser.Email = splittedData[1];
-                newUser.Password = splittedData[2];
-                newUser.Name = splittedData[3];
-                newUser.Surname = splittedData[4];
-                newUser.DisplayName = splittedData[5];
+                User newUser = new User(name: splittedData[3], surname: splittedData[4], email: splittedData[1], displayName: splittedData[5], password: splittedData[2]);
+                newUser.Id = Guid.Parse(splittedData[0]);
                 users.Add(newUser);
             }
             return users;
@@ -45,7 +40,7 @@ namespace YourScheduler.BusinessLogic
             string[] linesToCSV = new string[users.Count];
             for (int i = 0; i < users.Count; i++)
             {
-                linesToCSV[i] = Convert.ToString(users[i].Id) + ",";
+                linesToCSV[i] = users[i].Id.ToString() + ",";
                 linesToCSV[i] += users[i].Email + ",";
                 linesToCSV[i] += users[i].Password + ",";
                 linesToCSV[i] += users[i].Name + ",";
@@ -53,6 +48,57 @@ namespace YourScheduler.BusinessLogic
                 linesToCSV[i] += users[i].DisplayName;
             }
             File.WriteAllLines(GetUsersFilePath(), linesToCSV);
+        }
+        public static List<Event> GetEvents()
+        {
+            List<Event> events = new List<Event>();
+            string[] linesFromCSV = System.IO.File.ReadAllLines(GetEventsFilePath());
+            foreach (var line in linesFromCSV)
+            {
+                string[] splittedData = line.Split(',');
+                Guid newEventID = Guid.Parse(splittedData[0]);
+                DateTime newEventDate = DateTime.Parse(splittedData[3]);
+                List <Guid> newEventParticipants = new List<Guid>();
+
+                if(splittedData[4] != "")
+                {
+                    string[] splittedEventParticipants = splittedData[4].Split('|');
+                    foreach (var participantId in splittedEventParticipants)
+                    {
+                        newEventParticipants.Add(Guid.Parse(participantId));
+                    }
+                }
+                bool newEventIsOpen = Convert.ToBoolean(splittedData[5]);
+                Event newEvent = new Event(name: splittedData[1], description: splittedData[2], date: newEventDate, participants: newEventParticipants, isopen: newEventIsOpen);
+                newEvent.Id = newEventID;
+                events.Add(newEvent);
+            }
+            return events;
+        }
+        public static void UpdateEvents(List<Event> events)
+        {
+            string[] linesToCSV = new string[events.Count];
+            for (int i = 0; i < events.Count; i++)
+            {
+                linesToCSV[i] = events[i].Id.ToString() + ",";
+                linesToCSV[i] += events[i].Name + ",";
+                linesToCSV[i] += events[i].Description + ",";
+                linesToCSV[i] += events[i].Date.ToString() + ",";
+                for (int j = 0; j < events[i].Participants.Count; j++)
+                {
+                    if (j == events[i].Participants.Count-1)
+                    {
+                        linesToCSV[i] += events[i].Participants[j].ToString() + ",";
+
+                    }
+                    else
+                    {
+                        linesToCSV[i] += events[i].Participants[j].ToString() + "|";
+                    }
+                }
+                linesToCSV[i] += events[i].IsOpen.ToString();
+            }
+            File.WriteAllLines(GetEventsFilePath(), linesToCSV);
         }
     }
 }
