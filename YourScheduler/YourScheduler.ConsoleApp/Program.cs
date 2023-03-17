41654e8a449a847c180c5164cd7f57094a55a1c8
Mariusz Pudlik <marpudlik@wp.pl>
2023-03-17 22:51:44 +0100

    changes i main program

--- YourScheduler/YourScheduler.ConsoleApp/Program.cs
+++ YourScheduler/YourScheduler.ConsoleApp/Program.cs
@@ -13,7 +13,7 @@ namespace YourScheduler
     {
         static void Main(string[] args)
         {
-            User Kamil = new User(0, "Kamil", "Majewski", "SomeMail@Mail.com", "Milva", "HiddenPW213");
+            User Kamil = new User("Kamil", "Majewski", "SomeMail@Mail.com", "Milva", "HiddenPW213");
             Kamil.ChangeEmail("   nnnn ");
             Console.WriteLine(Kamil.Email);
             Program program = new Program();
