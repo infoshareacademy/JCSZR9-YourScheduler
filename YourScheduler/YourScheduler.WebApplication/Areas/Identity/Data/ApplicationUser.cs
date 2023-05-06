b3a8cf25bb6faa051e7b8385ce66a91b985a39dd
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-05-06 09:11:07 +0200

    Added Identity to Project, configured Login and Register Page, Created Identity DB

---
+++ YourScheduler/YourScheduler.WebApplication/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using YourScheduler.BusinessLogic;
+
+namespace YourScheduler.WebApplication.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string DisplayName { get; set; }
+
+}
+
