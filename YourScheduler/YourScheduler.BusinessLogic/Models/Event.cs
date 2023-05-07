bd1a91a6ceb6de2fad06b3cd87ed0c81f2d5b732
Mariusz Pudlik <marpudlik@wp.pl>
2023-05-07 13:04:42 +0200

    dsssd

---
+++ YourScheduler/YourScheduler.BusinessLogic/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YourScheduler.BusinessLogic.Models;
+
+public partial class Event
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public DateTime Date { get; set; }
+
+    public bool Isopen { get; set; }
+
+    public virtual ICollection<UsersEvent> UsersEvents { get; } = new List<UsersEvent>();
+}
---
+++ YourScheduler/YourScheduler.BusinessLogic/Services/UsersServiceForView.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using YourScheduler.BusinessLogic;
+using YourScheduler.WebApplication.Models;
+
+namespace YourScheduler.WebApplication.Services
+{
+    public class UsersServiceForView
+    {
+        UserService _userService = new UserService();
+        public List<UserModel> MapToMvc()
+        {
+            var modelUsers = _userService.GetUsers();
+
+            List<UserModel> users = new List<UserModel>();
+            foreach (var item in modelUsers)
+            {
+
+                UserModel userModel = new UserModel();
+                userModel.Id = item.Id;
+                userModel.Name = item.Name;
+                userModel.Email = item.Email;
+                userModel.Password = item.Password;
+                userModel.Displayname = item.Displayname;
+                users.Add(userModel);
+            }
+
+            return users;
+        }
+
+        public YourScheduler.BusinessLogic.Models.User  MapToModel(UserModel userModel) 
+        {
+            YourScheduler.BusinessLogic.Models.User user = new YourScheduler.BusinessLogic.Models.User();
+            
+            user.Name = userModel.Name;
+            user.Surname = userModel.Surname;
+            user.Displayname = userModel.Displayname;
+            user.Email = userModel.Email;
+            user.Password = userModel.Password;
+            
+              
+
+            return user;
+        }
+
+        public void AddUser(UserModel userModel) 
+        {
+           // YourScheduler.BusinessLogic.Models.User user = MapToModel(userModel);
+            _userService.AddUserAsync(MapToModel(userModel));
+
+        }
+
+        public YourScheduler.WebApplication.Models.UserModel GetUserById(int id) 
+        {
+           var users = MapToMvc();
+           var user= users.FirstOrDefault(u=>u.Id==id);
+           return user;
+        }
+    }
+}
+
