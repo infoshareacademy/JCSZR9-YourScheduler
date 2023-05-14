6a4f1ba718d9a214d1b5fcdb920dfe7f621a1abb
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-05-14 13:56:09 +0200

    Changed Models, Entities, Created a new Databaase called YourScheduler2, Changed Id to int from GUID, Migrated NetApp and Infrastructure to Net.7

--- YourScheduler/YourScheduler.Infrastructure/Entities/UserEvent.cs
+++ YourScheduler/YourScheduler.Infrastructure/Entities/UserEvent.cs
@@ -6,13 +6,13 @@ namespace YourScheduler.Infrastructure.Entities;
 
 public partial class UserEvent
 {
-    public int Id { get; set; }
-
-    public string UserId { get; set; }
+    public int UserEventId { get; set; }
 
     public int EventId { get; set; }
 
     public virtual Event Event { get; set; } = null!;
+
+    public int ApplicationUserId { get; set; }
 
     public virtual ApplicationUser ApplicationUser { get; set; } = null!;
 }
--- YourScheduler/YourScheduler.Infrastructure/YourSchedulerDbContext.cs
+++ YourScheduler/YourScheduler.Infrastructure/YourSchedulerDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,7 @@ using YourScheduler.Infrastructure.Entities;
 
 namespace YourScheduler.Infrastructure
 {
-    public class YourSchedulerDbContext:IdentityDbContext<ApplicationUser>
+    public class YourSchedulerDbContext:IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
     {
         public virtual DbSet<Event> Events { get; set; }
 
@@ -28,22 +29,15 @@ namespace YourScheduler.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            
+
             base.OnModelCreating(builder);
 
 
-            builder.Entity<UserTeam>().HasKey(ut => new { ut.UserId, ut.TeamId });
+            builder.Entity<UserTeam>().HasKey(ut => new { ut.ApplicationUserId, ut.TeamId });
 
-            builder.Entity<UserTeam>().HasOne<ApplicationUser>(us => us.ApplicationUser).WithMany(ut => ut.UsersTeams).HasForeignKey(us => us.UserId);
 
-            builder.Entity<UserTeam>().HasOne<Team>(t => t.Team).WithMany(ut => ut.UsersTeams).HasForeignKey(t => t.TeamId);
+            builder.Entity<UserEvent>().HasKey(ue => new { ue.ApplicationUserId, ue.EventId });
 
-            
-            builder.Entity<UserEvent>().HasKey(ue => new { ue.UserId, ue.EventId});
-
-            builder.Entity<UserEvent>().HasOne<ApplicationUser>(us => us.ApplicationUser).WithMany(ut => ut.UsersEvents).HasForeignKey(us => us.UserId);
-
-            builder.Entity<UserEvent>().HasOne<Event>(e => e.Event).WithMany(ut => ut.UsersEvents).HasForeignKey(e => e.EventId);
 
 
 
