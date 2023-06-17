95e074faac7925389a0937fba0b653ec428fed58
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-06-17 15:42:02 +0200

    More translations for validations

--- YourScheduler/YourScheduler.WebApplication/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ YourScheduler/YourScheduler.WebApplication/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -66,7 +66,7 @@ namespace YourScheduler.WebApplication.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required (ErrorMessage = "Pole email nie może być puste")]
             [EmailAddress]
             public string Email { get; set; }
 
@@ -74,7 +74,7 @@ namespace YourScheduler.WebApplication.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required (ErrorMessage = "Pole hasło nie może być puste")]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
