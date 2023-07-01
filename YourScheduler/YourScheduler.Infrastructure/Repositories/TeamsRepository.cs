f4abd88f4ba53de1e9f2c8287fba5d2203f56436
Marcin Dylowicz <marcindyl95@gmail.com>
2023-07-01 22:50:36 +0200

    Change Teams methods into asynchronous

--- YourScheduler/YourScheduler.Infrastructure/Repositories/TeamsRepository.cs
+++ YourScheduler/YourScheduler.Infrastructure/Repositories/TeamsRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using YourScheduler.Infrastructure.Entities;
 using YourScheduler.Infrastructure.Repositories.Interfaces;
 
@@ -17,91 +13,60 @@ namespace YourScheduler.Infrastructure.Repositories
             _dbContext = dbContext;
         }
 
-        public void AddTeam(Team team)
+        public async Task AddTeamAsync(Team team)
         {
-            _dbContext.Teams.Add(team);
+            await _dbContext.Teams.AddAsync(team);
+            await _dbContext.SaveChangesAsync();
         }
 
-        public void SaveData()
+        public async Task<List<Team>> GetAllExistedTeamsAsync()
         {
-            _dbContext.SaveChanges();
+            return await _dbContext.Teams.ToListAsync();
         }
 
-        public List<Team> GetAllExistedTeams()
+        public async Task<Team?> GetTeamByIdAsync(int id)
         {
-            List<Team> teams = new List<Team>();
-            teams = _dbContext.Teams.ToList();
-            return teams;
+            return await _dbContext.Teams.FirstOrDefaultAsync(t => t.TeamId == id);  
         }
 
-        public Team GetTeamById(int id)
+        public async Task DeleteTeamByIdAsync(int id)
         {
-            return _dbContext.Teams.FirstOrDefault(t=>t.TeamId==id);  
+            await _dbContext.Teams.Where(t => t.TeamId == id).ExecuteDeleteAsync();
+            await _dbContext.ApplicationUsersTeams.Where(t => t.TeamId == id).ExecuteDeleteAsync();
         }
 
-        public void DeleteTeamById(int id)
+        public async Task DeleteTeamFromCalendarByIdAsync(int id, int userId)
         {
-            var teamToDelete = GetTeamById(id);
-            if (teamToDelete != null)
-            {
-                _dbContext.Teams.Remove(teamToDelete);
-                var applicationUserTeamsToDelete = _dbContext.ApplicationUsersTeams.Where(x => x.TeamId == teamToDelete.TeamId);
-                _dbContext.ApplicationUsersTeams.RemoveRange(applicationUserTeamsToDelete);
-                _dbContext.SaveChanges();
-            }
+            await _dbContext.ApplicationUsersTeams.Where(x => x.TeamId == id && x.ApplicationUserId == userId).ExecuteDeleteAsync();
         }
 
-        public void DeleteTeamFromCalendarById(int id, int userId)
+        public async Task UpdateTeamAsync(Team teamToBase)
         {
-            var teamToDelete = GetTeamById(id);
-            if (teamToDelete != null)
-            {
-                var applicationUsersTeamsToDelete = _dbContext.ApplicationUsersTeams.SingleOrDefault(x => x.TeamId ==teamToDelete.TeamId && x.ApplicationUserId == userId);
-                _dbContext.ApplicationUsersTeams.Remove(applicationUsersTeamsToDelete);
-                _dbContext.SaveChanges();
-            }
+            await _dbContext.Teams.Where(t => t.TeamId == teamToBase.TeamId).ExecuteUpdateAsync(setters => setters
+            .SetProperty(t => t.Name, teamToBase.Name)
+            .SetProperty(t => t.Description, teamToBase.Description));
         }
 
-        public void UpdateTeam(Team teamToBase)
+        public async Task AddTeamForUserAsync(int applicationUserId, int teamId)
         {
-            var teamToUpdate = _dbContext.Teams.SingleOrDefault(e => e.TeamId == teamToBase.TeamId);
-            if (teamToUpdate != null)
-            {
-                teamToUpdate.Name = teamToBase.Name;
-                teamToUpdate.Description = teamToBase.Description;
-                
-                _dbContext.SaveChanges();
-            }
-        }
-
-        public void AddTeamForUser(int applicationUserId, int teamId)
-        {
-            _dbContext.ApplicationUsersTeams.Add(new ApplicationUserTeam { ApplicationUserId = applicationUserId, TeamId = teamId });
+            await _dbContext.ApplicationUsersTeams.AddAsync(new ApplicationUserTeam { ApplicationUserId = applicationUserId, TeamId = teamId });
+            await _dbContext.SaveChangesAsync();
         }
        
 
-        public List<Team> GetTeamsForUser(int applicationUserId)
+        public async Task<List<Team>> GetTeamsForUserAsync(int applicationUserId)
         {
-            List<int> ids = new List<int>();
-            List<Team> teams = new List<Team>();
-
-            teams = _dbContext.ApplicationUsersTeams.Where(x => x.ApplicationUserId == applicationUserId).Select(x => x.Team).ToList();
-
-            return teams;
-
+            return await _dbContext.ApplicationUsersTeams.Where(x => x.ApplicationUserId == applicationUserId).Select(x => x.Team).ToListAsync();
         }
 
-        public List<ApplicationUser> GetApplicationUsersForTeam(int teamId)
+        public async Task<List<ApplicationUser>> GetApplicationUsersForTeamAsync(int teamId)
         {
-            List<ApplicationUser> applicationUsers = new List<ApplicationUser>();
-            applicationUsers = _dbContext.ApplicationUsersTeams.Where(x => x.TeamId == teamId).Select(x => x.ApplicationUser).ToList();
-            return applicationUsers;
+            return await _dbContext.ApplicationUsersTeams.Where(x => x.TeamId == teamId).Select(x => x.ApplicationUser).ToListAsync();
         }
 
-        public bool CheckIfLoggedUserIsParticipant(int loggedUserId, int teamId)
+        public async Task<bool> CheckIfLoggedUserIsParticipantAsync(int loggedUserId, int teamId)
         {
-            var isLoggedUserParticipant = _dbContext.ApplicationUsersTeams.Any(e => e.ApplicationUserId == loggedUserId && e.TeamId == teamId);
-            return isLoggedUserParticipant;
+            return await _dbContext.ApplicationUsersTeams.AnyAsync(e => e.ApplicationUserId == loggedUserId && e.TeamId == teamId);
         }
     }
 
