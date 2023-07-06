ebc14e5bf3f4a870ba06d5a3dab491009e32cdde
Mariusz Pudlik <marpudlik@wp.pl>
2023-07-06 09:54:37 +0200

    Created context generator for repositories tests. Created three tests for repositories

---
+++ YourScheduler/YourScheduler.Infrastructure.xUnitTests/RepositoriesTests/ContextGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourScheduler.Infrastructure.xUnitTests.RepositoriesTests
+{
+    public static class ContextGenerator
+    {
+        public static YourSchedulerDbContext Generate()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<YourSchedulerDbContext>().UseInMemoryDatabase("HelperBase");
+
+            return new YourSchedulerDbContext(optionBuilder.Options);
+        }
+    }
+}
