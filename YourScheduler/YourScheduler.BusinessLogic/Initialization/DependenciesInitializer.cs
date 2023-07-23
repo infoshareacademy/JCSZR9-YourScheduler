672bccc0d4e4f2e34fef6049c10200100e38d364
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-07-23 12:57:59 +0200

    Changed email service from transient to singleton

--- YourScheduler/YourScheduler.BusinessLogic/Initialization/DependenciesInitializer.cs
+++ YourScheduler/YourScheduler.BusinessLogic/Initialization/DependenciesInitializer.cs
@@ -28,7 +28,7 @@ namespace YourScheduler.BusinessLogic.Initialization
             serviceCollection.AddScoped<ITeamMapper, TeamMapper>();
             serviceCollection.AddScoped<IHomeViewService, HomeViewService>();
             serviceCollection.AddScoped<IHomeViewMapper, HomeViewMapper>();
-            serviceCollection.AddTransient<IEmailService, EmailService>();         
+            serviceCollection.AddSingleton<IEmailService, EmailService>();         
         }
     }
 }
