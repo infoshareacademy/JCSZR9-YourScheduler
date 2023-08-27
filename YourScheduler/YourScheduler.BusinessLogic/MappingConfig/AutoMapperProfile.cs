bf52a51e97c4aff8b24ddb61d5548f8d98b7f078
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-08-27 21:08:13 +0200

    Added more mapping options to AutomapperProfile mismatched names between models

--- YourScheduler/YourScheduler.BusinessLogic/MappingConfig/AutoMapperProfile.cs
+++ YourScheduler/YourScheduler.BusinessLogic/MappingConfig/AutoMapperProfile.cs
@@ -25,13 +25,19 @@ namespace YourScheduler.BusinessLogic.MappingConfig
 
             CreateMap<ApplicationUser, ApplicationUserDto>();
 
-            CreateMap<TeamMembersDto, Team>();
+            CreateMap<TeamMembersDto, Team>()
+                .ForMember(dest => dest.TeamId, opt => opt.MapFrom(src => src.Id));
 
-            CreateMap<Team, TeamMembersDto>();
+            CreateMap<Team, TeamMembersDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.TeamId));
 
-            CreateMap<EventMembersDto, Event>();
+            CreateMap<EventMembersDto, Event>()
+                .ForMember(dest => dest.AdministratorId, opt => opt.MapFrom(src => src.administratorId))
+                .ForMember(dest => dest.IsOpen, opt => opt.MapFrom(src => src.Isopen));
 
-            CreateMap<Event, EventMembersDto>();
+            CreateMap<Event, EventMembersDto>()
+                .ForMember(dest => dest.administratorId, opt => opt.MapFrom(src => src.AdministratorId))
+                .ForMember(dest => dest.Isopen, opt => opt.MapFrom(src => src.IsOpen));
 
 
         }
